a681bd44e0fbf66ce9c65dee95aa643d94a9e033
Yohann <102670831+YohannMgt@users.noreply.github.com>
2023-03-27 17:24:23 +0200

    [FIX] coding style

--- include/prototype.h
+++ include/prototype.h
@@ -27,27 +27,4 @@
     /* event */
     int main_event(app_t *app);
 
-<<<<<<< Updated upstream
-    /* map*/
-    tile_t *create_tile(int id, manifest_t *manifest, sfVector2i position);
-    map_t *load_map_from_file(const char *pathfile, manifest_t *manifest);
-    int display_map(sfRenderWindow *window, map_t *map);
-
-    /* manifest */
-    int get_collision(manifest_t *manifest, int id);
-    sfTexture *get_texture(manifest_t *manifest, int id);
-
-    manifest_t *load_manifest(const char *filepath);
-    manifest_t *create_manifest(int id, char *path, int collision);
-    manifest_t *append_manifest(manifest_t *manifest, int id, char *path,
-        int collision);
-
-    /* array manipulation */
-    int print_array(char **array);
-    int len_array(char **array);
-    int free_array(char **array);
-    char **split(char *arg, char separator);
-
-=======
->>>>>>> Stashed changes
 #endif //BASE_REPO_PROTOTYPE_H
