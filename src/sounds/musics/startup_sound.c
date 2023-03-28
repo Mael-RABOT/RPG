57c22368ef88fddaa4bfb138383d26a341481710
Maël RABOT <mael.rabot@epitech.eu>
2023-03-28 15:13:14 +0200

    Add quit button
    Signed-off-by: Maël RABOT <mael.rabot@epitech.eu>

---
+++ src/sounds/musics/startup_sound.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** B-MUL-200-LYN-2-1-myrpg-mathieu.borel
+** File description:
+** startup_sound
+*/
+
+#include "../../include/include.h"
+
+int launch_startup_sound(app_t *app)
+{
+    ;
+}
