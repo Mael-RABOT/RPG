bbd79f982affb535724067543050d404944df3c3
Maël RABOT <mael.rabot@epitech.eu>
2023-03-28 16:00:19 +0200

    Update music player
    Signed-off-by: Maël RABOT <mael.rabot@epitech.eu>

--- src/sounds/musics/startup_sound.c
+++ src/sounds/musics/startup_sound.c
@@ -9,11 +9,24 @@
 
 int launch_startup_sound(app_t *app)
 {
-    app->sound_buffer = sfSoundBuffer_createFromFile(INTRO_MUSIC);
+    /*app->sound_buffer = sfSoundBuffer_createFromFile(INTRO_MUSIC);
     app->music = sfSound_create();
     sfSound_setBuffer(app->music, app->sound_buffer);
     sfSound_setVolume(app->music, 100);
     sfSound_play(app->music);
-    sfSound_setLoop(app->music, sfTrue);
+    sfSound_setLoop(app->music, sfTrue);*/
+    sfMusic *music = sfMusic_createFromFile(INTRO_MUSIC);
+    if (!music) {
+        printf("ERROR\n");
+        return 1;
+    }
+    sfMusic_setVolume(music, 100.0f);
+    sfMusic_play(music);
+    if (sfMusic_getStatus(music) != sfPlaying)
+    {
+        printf("Failed to play music\n");
+        sfMusic_destroy(music);
+        return 1;
+    }
     return (EXIT_SUCCESS);
 }
