0a661598f0593c1073d21586b9544ecd8c470e64
Yohann <102670831+YohannMgt@users.noreply.github.com>
2023-03-29 14:29:12 +0200

    [UPDATE] assets

--- include/my_macros.h
+++ include/my_macros.h
@@ -8,8 +8,18 @@
 #ifndef MY_MACROS_H_
     #define MY_MACROS_H_
 
+    #define BACKGROUNDS         "assets/backgrounds/"
+    #define BUTTONS             "assets/buttons/main_menu/"
+    #define MENU_SETTINGS       "assets/buttons/main_menu/settings/"
+    #define CINEMATICS          "assets/cinematics/"
+    #define FONTS               "assets/fonts/"
     #define IMAGES              "assets/images/"
-    #define BUTTON              "assets/buttons/main_menu"
+    #define IRON                "assets/iron/"
+    #define NATURE              "assets/nature/"
+    #define NEON                "assets/nature/"
+    #define EFFECTS             "assets/sounds/effects/"
+    #define MUSICS              "assets/sounds/musics/"
+    #define TITLES              "assets/titles/"
 
     #define RUINED_BACK         IMAGES "ruined_back.png"
     #define RUINED_FORE         IMAGES "ruined_fore.png"
@@ -17,10 +27,10 @@
     #define SKIP                IMAGES "skip.png"
     #define RUINED_CITY         IMAGES "ruined_city.png"
 
-    #define PLAY                BUTTON "play.png"
-    #define RESUME              BUTTON "resume.png"
-    #define SETTINGS            BUTTON "settings.png"
-    #define QUIT                BUTTON "quit.png"
+    #define PLAY                BUTTONS "play.png"
+    #define RESUME              BUTTONS "resume.png"
+    #define SETTINGS            BUTTONS "settings.png"
+    #define QUIT                BUTTONS "quit.png"
 
     #define RUINED_FORE_WIDTH   2880
     #define RUINED_BACK_WIDTH   2880
