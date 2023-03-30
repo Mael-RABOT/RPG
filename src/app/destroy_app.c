fd268ab457ee2fe507b424d6bebff2690c282071
Maël RABOT <mael.rabot@epitech.eu>
2023-03-30 23:36:46 +0200

    Add particle_list field ddestruction
    Signed-off-by: Maël RABOT <mael.rabot@epitech.eu>

--- src/app/destroy_app.c
+++ src/app/destroy_app.c
@@ -7,11 +7,33 @@
 
 #include "../../include/prototype.h"
 
+/*
+ *     typedef struct particle {
+        sfTexture *texture;
+        sfSprite *sprite;
+        sfVector2f position;
+        int frame;
+        int max_frame;
+    } particle_t;
+ *
+ * */
+
+void destroy_particle(app_t *app)
+{
+    for (int i = 0; app->particle_list[i] != NULL; i++) {
+        sfTexture_destroy(app->particle_list[i]->texture);
+        sfSprite_destroy(app->particle_list[i]->sprite);
+        free(app->particle_list[i]);
+    }
+    free(app->particle_list);
+}
+
 int destroy_app(app_t *app)
 {
     destroy_background(app);
     sfMusic_destroy(app->music);
     sfRenderWindow_destroy(app->window);
+    destroy_particle(app);
     free(app);
     return 0;
 }
