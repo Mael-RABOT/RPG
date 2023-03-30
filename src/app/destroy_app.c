b7f4a30190c862b61b83808f894b73c9ee9c9f1b
Maël RABOT <mael.rabot@epitech.eu>
2023-03-30 23:31:52 +0200

    Add cursor field destruction
    Signed-off-by: Maël RABOT <mael.rabot@epitech.eu>

--- src/app/destroy_app.c
+++ src/app/destroy_app.c
@@ -7,11 +7,19 @@
 
 #include "../../include/prototype.h"
 
+void destroy_sprite_t(my_sprite_t *sprite)
+{
+    sfTexture_destroy(sprite->texture);
+    sfSprite_destroy(sprite->sprite);
+    free(sprite);
+}
+
 int destroy_app(app_t *app)
 {
     destroy_background(app);
     sfMusic_destroy(app->music);
     sfRenderWindow_destroy(app->window);
+    destroy_sprite_t(app->cursor);
     free(app);
     return 0;
 }
