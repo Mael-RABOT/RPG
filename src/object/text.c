d7c89430eb0f29b1ceddde41ede80074f50a9a83
Your Name <mathieu.borel@epitech.eu>
2023-04-06 17:23:02 +0200

    edit for cs

--- src/object/text.c
+++ src/object/text.c
@@ -7,14 +7,6 @@
 
 #include "../../include/prototype.h"
 
-int destroy_text(text_t *text)
-{
-    //sfSprite_destroy(sprite->sprite);
-    //sfTexture_destroy(sprite->texture);
-    //free(sprite);
-    return 0;
-}
-
 text_t *create_text(const char *pathfile, int size, sfColor color, char *texts)
 {
     text_t *text = malloc(sizeof(text_t));
