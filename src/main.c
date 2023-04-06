8c56ce871b749105b21e006a18bd88b18aed2973
Maël RABOT <mael.rabot@epitech.eu>
2023-04-06 09:23:44 +0200

    Add help flag
    Signed-off-by: Maël RABOT <mael.rabot@epitech.eu>

--- src/main.c
+++ src/main.c
@@ -7,8 +7,34 @@
 
 #include "../include/prototype.h"
 
+static int display_help(void)
+{
+    FILE *stream = fopen("./help", "r");
+    if (!stream)
+        return 1;
+    size_t len;
+    char *line = NULL;
+    while (getline(&line, &len, stream) != -1)
+        my_printf(line);
+    fclose(stream);
+    free(line);
+    return 0;
+}
+
+static int parsing(const int ac, const char **av)
+{
+    for (int i = 1; i < ac; i++) {
+        if (my_strcmp(av[i], "-h") == 0)
+            return display_help();
+    }
+    return -1;
+}
+
 int main(const int ac, const char **av)
 {
+    int parsing_value = parsing(ac, av);
+    if (parsing_value != -1)
+        return (parsing_value);
     srand(time(NULL));
     //generator(30, 30, 1);
     return my_rpg(ac, av);
