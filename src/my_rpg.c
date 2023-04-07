28db794e666788ea4849bb92e586671dc4b59250
Maël RABOT <mael.rabot@epitech.eu>
2023-04-07 15:35:01 +0200

    Update maps
    Signed-off-by: Maël RABOT <mael.rabot@epitech.eu>

--- src/my_rpg.c
+++ src/my_rpg.c
@@ -39,10 +39,10 @@ int my_rpg(const int ac, const char **av)
     app_t *app = create_app(videomode);
     app->default_view = create_view((sfVector2f){1920, 1080});
     app->view = create_view((sfVector2f){426, 240});
-    app->player = create_entity(app, "./assets/characters/small_knight.png",
+    app->player = create_entity(app, "./assets/characters/player_debug.png",
         Default, Fist);
     app->maps = load_maps();
-    change_map(app, app->maps, app->player, 6);
+    change_map(app, app->maps, app->player, 0);
     while (sfRenderWindow_isOpen(app->window)) {
         main_display(app);
     }
