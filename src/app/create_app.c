ffa74b712430fc222ae11c480e3fc8e423945503
mathieu <mathieu.borel@epitech.eu>
2023-04-19 15:39:58 +0200

    edit map

--- src/app/create_app.c
+++ src/app/create_app.c
@@ -41,7 +41,7 @@ app_t *create_app(sfVideoMode videomode)
     create_weapons_list(app);
     app->videomode = (sfVideoMode){1920, 1080, 32};
     app->window = create_window(app->videomode, "my_rpg");
-    sfRenderWindow_setFramerateLimit(app->window, 60);
+    sfRenderWindow_setFramerateLimit(app->window, 120);
     app->fps = create_fps_counter();
     app->background = create_sprite(NIGHT_SKY);
     create_cursor(app);
