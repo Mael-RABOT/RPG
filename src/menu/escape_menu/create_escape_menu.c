e975e441a83e403f7406d73d89dcb8ccea6bb4d4
Mathieu BOREL <mathieu.borel@epitech.eu>
2023-04-20 18:01:27 +0200

    edit for cs

--- src/menu/escape_menu/create_escape_menu.c
+++ src/menu/escape_menu/create_escape_menu.c
@@ -12,16 +12,16 @@ escape_menu_t *create_escape_menu(sfRenderWindow *window)
     sfVector2u window_size = sfRenderWindow_getSize(window);
     escape_menu_t *escape_menu = malloc(sizeof(escape_menu_t));
     escape_menu->button = malloc(sizeof(button_t *) * 4);
-    object_info_t button_info;
-    memset(&button_info, 0, sizeof(button_info));
-    button_info.scale = (sfVector2f){1, 1};
-    button_info.size = (sfVector2f){64 * 3, 32 * 3};
-    button_info.position = (sfVector2f){window_size.x / 2 - button_info.size.x / 2, 300};
-    escape_menu->button[0] = create_button(button_info, &play, RESUME);
-    button_info.position = (sfVector2f){window_size.x / 2 - button_info.size.x / 2, 400};
-    escape_menu->button[1] = create_button(button_info, &launch_setting, SETTING);
-    button_info.position = (sfVector2f){window_size.x / 2 - button_info.size.x / 2, 500};
-    escape_menu->button[2] = create_button(button_info, &launch_main_menu, MAIN_MENU);
+    object_info_t bi;
+    memset(&bi, 0, sizeof(bi));
+    bi.scale = (sfVector2f){1, 1};
+    bi.size = (sfVector2f){64 * 3, 32 * 3};
+    bi.position = (sfVector2f){window_size.x / 2 - bi.size.x / 2, 300};
+    escape_menu->button[0] = create_button(bi, &play, RESUME);
+    bi.position = (sfVector2f){window_size.x / 2 - bi.size.x / 2, 400};
+    escape_menu->button[1] = create_button(bi, &launch_setting, SETTING);
+    bi.position = (sfVector2f){window_size.x / 2 - bi.size.x / 2, 500};
+    escape_menu->button[2] = create_button(bi, &launch_main_menu, MAIN_MENU);
     escape_menu->button[3] = NULL;
     return escape_menu;
 }
